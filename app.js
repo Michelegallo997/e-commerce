07e238f45a1da1fab895dd19763459a5d7fca746
Michele Gallo <michelegallo997@gmail.com>
2024-09-24 20:51:41 -0300

    e-commerce4

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 const btn = document.querySelector('.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
+let nextButton= document.getElementById('next');
+let prevButton= document.getElementById('prev');
+let backButton= document.getElementById('back');
+let seeMoreButton=document.querySelectorAll('.seeMore');
+let carrusel=document.querySelector('.carrusel');
+let listHTML=document.querySelector('.carrusel .list');
 
+nextButton.onclick = function(){
+    showSlider('next');
+}
+prevButton.onclick = function(){
+    showSlider('prev');
+}
+let unAcceppClick;
+const showSlider= (type) => { 
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+    carrusel.classList.remove('next', 'prev');
+    let items = document.querySelectorAll('.carrusel .list .item');
+    if(type === 'next'){
+    listHTML.appendChild(items[0]);
+    carrusel.classList.add('next');
+    }else{
+    let positionLast = items.length -1;
+    listHTML.prepend(items[positionLast]);
+    carrusel.classList.add('prev');
+    }
+    clearTimeout(unAcceppClick);
+    unAcceppClick = setTimeout(()=>{
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 2000)
+}
+seeMoreButton.forEach(button => {
+    button.onclick = function(){
+        carrusel.classList.add('showDetail');
+    }
+});
+backButton.onclick = function(){
+    carrusel.classList.remove('showDetail');
+}
 btn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
